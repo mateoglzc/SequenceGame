b7513d7347ea900b3dfcc85ac13a7ddcf9f4802c
Mateo Gonzalez Cosio <mateoglzc@hotmail.com>
2020-08-11 20:17:46 -0500

    Created grid

--- Sequence.py
+++ Sequence.py
@@ -12,6 +12,13 @@ screen_w = 800
 screen_h = 600
 screen = pg.display.set_mode((screen_w, screen_h))
 
+
+# Colors
+white = pg.Color(255, 255, 255)
+black = pg.Color(0, 0, 0)
+red = pg.Color("red")
+cool_grey = pg.Color(108, 117, 125)
+
 # Game loop
 while True:
     # Event checker
@@ -19,6 +26,30 @@ while True:
         if event.type == pg.QUIT:
             pg.quit()
             sys.exit()
+
+    screen.fill((237, 242, 244))
+
+    # Border Lines
+    pg.draw.line(screen, black, (50, 50), (50, 550), 4)
+    pg.draw.line(screen, black, (750, 50), (750, 550), 4)
+    pg.draw.line(screen, black, (50, 50), (750, 50), 4)
+    pg.draw.line(screen, black, (50, 550), (750, 550), 4)
+
+    # Card slots lines
+
+    vertical_line_adder = 150
+    horizontal_line_adder = 150
+
+    for _ in range(7):
+        pg.draw.line(screen, cool_grey,
+                     (vertical_line_adder, 50), (vertical_line_adder, 550), 4)
+        vertical_line_adder += 100
+
+    for _ in range(7):
+        pg.draw.line(screen, cool_grey,
+                     (50, horizontal_line_adder), (750, horizontal_line_adder),
+                     4)
+        horizontal_line_adder += 100
 
     # Upadte the full surface display
     pg.display.flip()
