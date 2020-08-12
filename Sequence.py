025fbbec402c698a9404546fea797686e5000f6d
Mateo Gonzalez Cosio <mateoglzc@hotmail.com>
2020-08-11 21:56:55 -0500

    Added Colors of cards, board is missing one h line

--- Sequence.py
+++ Sequence.py
@@ -1,3 +1,4 @@
+from cool_pgColors import cools
 import pygame as pg
 import sys
 
@@ -14,10 +15,9 @@ screen = pg.display.set_mode((screen_w, screen_h))
 
 
 # Colors
-white = pg.Color(255, 255, 255)
-black = pg.Color(0, 0, 0)
-red = pg.Color("red")
 cool_grey = pg.Color(108, 117, 125)
+cool_blue = pg.Color(152, 193, 217)
+
 
 # Game loop
 while True:
@@ -30,10 +30,10 @@ while True:
     screen.fill((237, 242, 244))
 
     # Border Lines
-    pg.draw.line(screen, black, (50, 50), (50, 550), 4)
-    pg.draw.line(screen, black, (750, 50), (750, 550), 4)
-    pg.draw.line(screen, black, (50, 50), (750, 50), 4)
-    pg.draw.line(screen, black, (50, 550), (750, 550), 4)
+    pg.draw.line(screen, cools["black"], (50, 50), (50, 550), 4)
+    pg.draw.line(screen, cools["black"], (750, 50), (750, 550), 4)
+    pg.draw.line(screen, cools["black"], (50, 50), (750, 50), 4)
+    pg.draw.line(screen, cools["black"], (50, 550), (750, 550), 4)
 
     # Card slots lines
 
@@ -50,6 +50,69 @@ while True:
                      (50, horizontal_line_adder), (750, horizontal_line_adder),
                      4)
         horizontal_line_adder += 100
+
+    # IDK how to make this more efficient
+
+    # # Free Spaces
+    # pg.draw.circle(screen, cool_blue, (100, 100), 35, 10)
+    # pg.draw.circle(screen, cool_blue, (100, 500), 35, 10)
+    # pg.draw.circle(screen, cool_blue, (700, 100), 35, 10)
+    # pg.draw.circle(screen, cool_blue, (700, 500), 35, 10)
+
+    # # Tiger
+    # pg.draw.circle(screen, cools["tiger"], (600, 500), 35)
+    # pg.draw.circle(screen, cools["tiger"], (200, 100), 35)
+    # # Kangaroo
+    # pg.draw.circle(screen, cools["kangaroo"], (500, 500), 35)
+    # pg.draw.circle(screen, cools["kangaroo"], (500, 200), 35)
+    # # Giraffe
+    # pg.draw.circle(screen, cools["giraffe"], (400, 500), 35)
+    # pg.draw.circle(screen, cools["giraffe"], (500, 300), 35)
+    # # Wolf
+    # pg.draw.circle(screen, cools["wolf"], (300, 500), 35)
+    # pg.draw.circle(screen, cools["wolf"], (300, 300), 35)
+    # # Turtle
+    # pg.draw.circle(screen, cools["turtle"], (200, 500), 35)
+    # pg.draw.circle(screen, cools["turtle"], (200, 200), 35)
+    # # Fox
+    # pg.draw.circle(screen, cools["fox"], (200, 400), 35)
+    # pg.draw.circle(screen, cools["fox"], (300, 300), 35)
+    # # Rendeer
+    # pg.draw.circle(screen, cools["rendeer"], (500, 100), 35)
+    # pg.draw.circle(screen, cools["rendeer"], (700, 300), 35)
+    # # Lion
+    # pg.draw.circle(screen, cools["lion"], (500, 300), 35)
+    # pg.draw.circle(screen, cools["lion"], (600, 100), 35)
+    # # Crocodile
+    # pg.draw.circle(screen, cools["crocodile"], (400, 500), 35)
+    # pg.draw.circle(screen, cools["crocodile"], (600, 300), 35)
+    # # Zebra
+    # pg.draw.circle(screen, cools["zebra"], (200, 400), 35)
+    # pg.draw.circle(screen, cools["zebra"], (300, 300), 35)
+    # # Monkey
+    # pg.draw.circle(screen, cools["monkey"], (200, 400), 35)
+    # pg.draw.circle(screen, cools["monkey"], (300, 300), 35)
+    # # Hippo
+    # pg.draw.circle(screen, cools["hippo"], (200, 400), 35)
+    # pg.draw.circle(screen, cools["hippo"], (300, 300), 35)
+    # # Panda
+    # pg.draw.circle(screen, cools["panda"], (200, 400), 35)
+    # pg.draw.circle(screen, cools["panda"], (300, 300), 35)
+    # # Shark
+    # pg.draw.circle(screen, cools["shark"], (200, 400), 35)
+    # pg.draw.circle(screen, cools["shark"], (300, 300), 35)
+    # # Camel
+    # pg.draw.circle(screen, cools["camel"], (200, 400), 35)
+    # pg.draw.circle(screen, cools["camel"], (300, 300), 35)
+    # # Orca
+    # pg.draw.circle(screen, cools["orca"], (200, 400), 35)
+    # pg.draw.circle(screen, cools["orca"], (300, 300), 35)
+    # # Elephant
+    # pg.draw.circle(screen, cools["elephant"], (200, 400), 35)
+    # pg.draw.circle(screen, cools["elephant"], (300, 300), 35)
+    # # Penguin
+    # pg.draw.circle(screen, cools["penguin"], (200, 400), 35)
+    # pg.draw.circle(screen, cools["penguin"], (300, 300), 35)
 
     # Upadte the full surface display
     pg.display.flip()
---
+++ cool_pgColors.py
@@ -0,0 +1,24 @@
+import pygame as pg
+
+cools = {
+    "tiger": pg.Color(242, 158, 76),
+    "kangaroo": pg.Color(241, 196, 83),
+    "giraffe": pg.Color(243, 114, 44),
+    "wolf": pg.Color(87, 117, 144),
+    "turtle": pg.Color(144, 190, 109),
+    "fox": pg.Color(248, 150, 30),
+    "rendeer": pg.Color(192, 133, 82),
+    "lion": pg.Color(137, 87, 55),
+    "crocodile": pg.Color(138, 201, 38),
+    "zebra": pg.Color(72, 74, 71),
+    "monkey": pg.Color(94, 84, 142),
+    "hippo": pg.Color(92, 109, 112),
+    "panda": pg.Color(79, 93, 117),
+    "shark": pg.Color(25, 133, 161),
+    "camel": pg.Color(218, 183, 133),
+    "orca": pg.Color(65, 90, 119),
+    "elephant": pg.Color(204, 218, 209),
+    "penguin": pg.Color(58, 110, 165),
+    "black": pg.Color(0, 0, 0),
+    "white": pg.Color(255, 255, 255),
+}
